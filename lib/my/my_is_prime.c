2cb4069a45ced601d1aa3afff6a42899d63ad3f9
iMeaNz <mehdy.morvan@epitech.eu>
2021-12-06 11:07:38 +0100

    Removed useless static function

--- lib/my/my_is_prime.c
+++ lib/my/my_is_prime.c
@@ -5,6 +5,8 @@
 ** Finds if a number is prime
 */
 
+int my_compute_power_it(int nb, int p);
+
 static int my_compute_power_it_prime(int nb, int p)
 {
     int result = 1;
@@ -21,7 +23,7 @@ int my_is_prime(int nb)
         return nb > 1 && nb != 4;
     if (nb % 2 == 0 || nb % 3 == 0)
         return 0;
-    for (int i = 6; my_compute_power_it_prime(i - 6, 2) <= nb; i += 6) {
+    for (int i = 6; my_compute_power_it(i - 6, 2) <= nb; i += 6) {
         if (nb % (i - 1) == 0 || nb % (i + 1) == 0) {
             return 0;
         }
